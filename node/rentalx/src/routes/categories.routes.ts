4c29200ced24aa82a1aa0c53976a0de4117626cc
Phablo Vilas Boas <phablovilasboas25@gmail.com>
2021-10-15 16:50:10 -0300

    subindo repo

--- node/rentalx/src/routes/categories.routes.ts
+++ node/rentalx/src/routes/categories.routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import multer from 'multer';
 
 import { createCategoryController } from '../modules/cars/useCases/createCategory';
+import { importCategoryController } from '../modules/cars/useCases/importCategory';
 import { listCategoriesController } from '../modules/cars/useCases/listCategories';
 
 const categoriesRoutes = Router();
@@ -12,11 +13,6 @@ const upload = multer({
 
 categoriesRoutes.post('/', (request, response) => createCategoryController.handle(request, response));
 categoriesRoutes.get('/', (request, response) => listCategoriesController.handle(request, response));
-
-categoriesRoutes.post('/import', upload.single('file'), (request, response) => {
-  const { file } = request;
-  console.log('file: ', file);
-  return response.send();
-});
+categoriesRoutes.post('/import', upload.single('file'), (request, response) => importCategoryController.handle(request, response));
 
 export { categoriesRoutes };
--- node/rentalx/src/server.ts
+++ node/rentalx/src/server.ts
@@ -1,9 +1,16 @@
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+
 import { router } from './routes';
+import swaggerFile from './swagger.json';
 
 const app = express();
+
 // Parse JSON
 app.use(express.json());
+
+// Swagger
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use(router);
 
 app.listen(3334, () => {
